1ed01e6109d7369e73aae7107757435a33f0cd39
susimsek <suaybsimsek58@gmail.com>
2022-12-04 19:58:15 +0300

    add big decimal scalar example

--- src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/controller/HelloController.kt
+++ src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/controller/HelloController.kt
@@ -6,6 +6,7 @@ import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Mono
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.OffsetDateTime
 
@@ -18,7 +19,8 @@ class HelloController {
         return Mono.just(
             Message(
                 content =  mutableMapOf("sivas" to "58", "istanbul" to "43"),
-                sentDate = LocalDate.now()
+                sentDate = LocalDate.now(),
+                price = BigDecimal(30.82)
             )
         ).awaitSingle()
     }
--- src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/type/Message.kt
+++ src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/type/Message.kt
@@ -1,9 +1,11 @@
 package io.github.susimsek.springgraphqlsamples.graphql.type
 
+import java.math.BigDecimal
 import java.time.LocalDate
 
 
 data class Message(
     val content: Map<String, Any>,
-    val sentDate: LocalDate
+    val sentDate: LocalDate,
+    val price: BigDecimal
 )
