e27fcda902c746b1f97d04e9841ec571a2f155f8
susimsek <suaybsimsek58@gmail.com>
2023-01-01 12:20:12 +0300

    add responseSanityCheck

--- src/main/kotlin/io/github/susimsek/springgraphqlsamples/security/recaptcha/RecaptchaService.kt
+++ src/main/kotlin/io/github/susimsek/springgraphqlsamples/security/recaptcha/RecaptchaService.kt
@@ -3,14 +3,20 @@ package io.github.susimsek.springgraphqlsamples.security.recaptcha
 import io.github.susimsek.springgraphqlsamples.exception.InvalidCaptchaException
 import io.github.susimsek.springgraphqlsamples.exception.RECAPTCHA_INVALID_MSG_CODE
 import kotlinx.coroutines.reactive.awaitSingle
-import org.slf4j.LoggerFactory
+import org.springframework.util.StringUtils
+import java.util.regex.Pattern
 
 
 class RecaptchaService(
     private val recaptchaClient: RecaptchaClient,
     private val recaptchaProperties: RecaptchaProperties
 ) {
+
+    private val responsePattern = Pattern.compile("[A-Za-z0-9_-]+")
     suspend fun validateToken(recaptchaToken: String): RecaptchaResponse {
+        if(!responseSanityCheck(recaptchaToken)) {
+            throw InvalidCaptchaException(RECAPTCHA_INVALID_MSG_CODE)
+        }
         return recaptchaClient.verifyResponse(recaptchaProperties.secretKey, recaptchaToken)
             .map {
                 when (it.success) {
@@ -18,5 +24,9 @@ class RecaptchaService(
                     else -> it
                 }
             }.awaitSingle()
+    }
+
+    private fun responseSanityCheck(response: String): Boolean {
+        return StringUtils.hasLength(response) && responsePattern.matcher(response).matches()
     }
 }
