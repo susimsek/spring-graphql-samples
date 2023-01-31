95dc1b84da248609625359cf9e41c750aa0e6203
susimsek <suaybsimsek58@gmail.com>
2023-01-31 19:08:44 +0300

    fix build error

---
+++ frontend/@types/i18next.d.ts
@@ -0,0 +1,14 @@
+/**
+ * If you want to enable locale keys typechecking and enhance IDE experience.
+ *
+ * Requires `resolveJsonModule:true` in your tsconfig.json.
+ *
+ * @link https://www.i18next.com/overview/typescript
+ */
+import 'i18next'
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    returnNull: false;
+  }
+}
