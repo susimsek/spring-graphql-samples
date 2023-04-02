812199ca849799078eb83a2c725ca5f30adc5f28
susimsek <suaybsimsek58@gmail.com>
2023-04-02 19:02:39 +0300

    fix nested property for native image

--- src/main/kotlin/io/github/susimsek/springgraphqlsamples/config/AppProperties.kt
+++ src/main/kotlin/io/github/susimsek/springgraphqlsamples/config/AppProperties.kt
@@ -5,17 +5,20 @@ import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.URL
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.validation.annotation.Validated
 
 @ConfigurationProperties("app")
 @Validated
 data class AppProperties(
     @field:Valid
+    @NestedConfigurationProperty
     val mail: Mail
 )
 
 data class Mail(
     @field:Valid
+    @NestedConfigurationProperty
     var from: MailFrom,
 
     @field:NotBlank
