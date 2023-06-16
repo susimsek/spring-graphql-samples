e6a549ffe02d5cacdda09e527dc5fb840c466fe4
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-06-16 09:03:13 +0300

    fix secret

--- src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/controller/HelloController.kt
+++ src/main/kotlin/io/github/susimsek/springgraphqlsamples/graphql/controller/HelloController.kt
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 import java.net.URI
